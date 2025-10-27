b7b2b4a1bb02009a8f5370386091815442f5c6c9
최지웅 <choijiwoong@choejiung-ui-MacBookAir.local>
2025-10-27 13:05:27 +0900

    ㅇㅇ

--- license/js/footer.js
+++ license/js/footer.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer style="line-height:140%;">
-            <img src="https://zipcare.kr/img/footer-logo.png" style="width:140px;background:transparent;"><br><br>
+            <img src="./img/footer-logo.png" style="width:140px;background:transparent;"><br><br>
             <span id="bz-name" style="line-height:140%;font-size:1rem;max-width:500px;">   (05801) 서울특별시 송파구 새말로 155 동화빌딩 3층
 대표이사 : 전미선 | 개인정보관리 책임자 : 전정운
 사업자등록번호 : 231-87-02923 | 이메일 : poet007@naver.com
